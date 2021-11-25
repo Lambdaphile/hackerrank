bf3145430199eab150ee957b4ea7282533b99702
λ • genesis <rwxgenesis@gmail.com>
2021-11-25 22:42:31 +0300

    Solve "compare the triplets" exercise

---
+++ algorithms/03-compare-the-triplets.js
@@ -0,0 +1,64 @@
+/**
+ * To verify solution and read challenge description check: https://www.hackerrank.com/challenges/compare-the-triplets/problem
+ */
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {
+    // Write your code here
+    return a.reduce((points, curr, i) => {
+        if (curr > b[i]) {
+            points[0]++;
+            return points;
+        } else if (b[i] > curr) {
+            points[1]++;
+            return points;
+        } else {
+            return points;
+        }
+    }, [0,0])
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
